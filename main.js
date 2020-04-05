fb0d5b3dc8044ef5de4a62b788604bb16c0741cd
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-04-05 23:28:43 +0200

    Add - Snack 5 - Bonus

--- main.js
+++ main.js
@@ -146,5 +146,67 @@ for (var i = 0; i < numbers.length; i++) {
 
 console.log('la somma dei numeri che nell\'array sono in posizione dispari è', somma);
 
+/////////////////////////////////////////////////
+
+console.log('SNACK 5 (BONUS++) - Confronto e popolazione Array');
+
+/**
+ * genera due array random
+ * confrontali e popolali in maniera uguale 
+*/
+var marco = [];
+var vincenzo = [];
+var differenza = 0;
+
+for (var i = 0; i < randomInt(); i++){
+  marco.push(randomInt());
+}
+
+for (var i = 0; i < randomInt(); i++){
+  vincenzo.push(randomInt());
+}
+
+console.log("caramelle marco " + marco.length);
+console.log("caramelle vincenzo " + vincenzo.length);
+
+if (marco.length > vincenzo.length) {
+
+  differenza = marco.length - vincenzo.length;
+
+  console.log("Marco ha "+ differenza + " caramella di differenza")
+
+  for (var j = 0; j < differenza; j++) {
+    
+    vincenzo.push(randomInt());
+  }
+
+  
+
+} else if(vincenzo.length > marco.length){
+
+  differenza = vincenzo.length - marco.length;
+
+  console.log("Vincenzo ha "+ differenza + " caramella di differenza")
+  for (var f = 0; f < differenza; f++){
+    marco.push(randomInt());
+  }
+
+  
+
+} else{
+
+  console.log("Marco e Vincenzo hanno le stesse caramelle")
+
+}
+
+console.log(vincenzo);
+console.log(marco);
 
 
+
+
+
+// Funzioni
+function randomInt(){
+  return Math.floor(Math.random() * 10)+1;
+}
